82ff8c666ee68f968c41155e8806b6ced49fa111
amrmohamedaboelnaser <amrmohamedaboelnaser@gmail.com>
2017-03-12 20:06:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
 -=[Mora Bot]=-
-</title><link rel="stylesheet" type="text/css" href="hd.css" media="all,handheld"/><link rel="shortcut icon" href="">
+</title><link rel="stylesheet" type="text/css" href="mora.css" media="all,handheld"/><link rel="shortcut icon" href="">
 
 <?php
 session_start();
